34a91bd6e8db3910f23467fbbef845e8b67ddd1b
boyyangzz <boyyzz@vip.qq.com>
2016-04-15 11:52:22 +0800

    Update and rename config.php to demo.php

--- demo.php
+++ demo.php
@@ -2,7 +2,14 @@
 define("REDIS_HOST", '127.0.0.1');//服务器IP
 define("REDIS_PASS", '123');//授权密码
 define("REDIS_PORT", 6379);
-
 #redis 限速配置
 define("REDIS_COUNT", 60);//次数
 define("REDIS_TIME", 7200);//时间 单位秒
+include('limiter_class.php');
+
+
+$userid=addslashes($_GET['userid']);
+$new=new limiter_class();
+
+$limit=$new->limit_redis($userid);
+$limit['error']==-1 ? echo '访问频繁,稍后尝试'; :  echo '正常访问';
