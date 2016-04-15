dc3b998d744dea57258c7658218899e686a065d1
boyyangzz <boyyzz@vip.qq.com>
2016-04-15 11:37:09 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+define("REDIS_HOST", '127.0.0.1');//服务器IP
+define("REDIS_PASS", '123');//授权密码
+define("REDIS_PORT", 6379);
+
+#redis 限速配置
+define("REDIS_COUNT", 60);//次数
+define("REDIS_TIME", 7200);//时间 单位秒
